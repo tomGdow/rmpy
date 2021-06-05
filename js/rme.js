02be743582b6db2714f3314152e211114522ea47
tomgdow <thomasgdowling@gmail.com>
2021-06-05 21:51:15 +0100

    create, modify and validate 176_boolean_op_and_or_not.html

--- js/rme.js
+++ js/rme.js
@@ -82,6 +82,7 @@ let pageData = (function () {
     "173_functions.html",
     "174_docstring.html",
     "175_dir.html",
+    "176_boolean_operations_and_or_not.html",
     "142_armstrong_number.html",
     "170_x_alphabet_sequence.html",
     "148_notes_jason_one.html",
