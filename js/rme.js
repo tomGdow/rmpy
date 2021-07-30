a9628d5397bb9c6898e102bb18fbaa3eeb5882ec
tomgdow <thomasgdowling@gmail.com>
2021-07-30 20:15:20 +0100

    193_os_os_os_dot_path_modules.html name to rme.js

--- js/rme.js
+++ js/rme.js
@@ -96,6 +96,7 @@ let pageData = (function () {
     "189_with.html",
     "190__main__and__name__.html",
     "191_sys_module.html",
+    "193_os_os_dot_path_modules.html",
     "148_notes_jason_one.html",
     "149_notes_jason_two.html",
     "154_notes_jason_three.html",
